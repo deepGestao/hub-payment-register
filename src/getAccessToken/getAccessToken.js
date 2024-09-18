e3bb837809d00c1619bb62ac4ef9be5f2269303d
k.tavares.martins <k.tavares.martins@accenture.com>
2024-09-18 19:21:21 -0300

    add test env

--- src/getAccessToken/getAccessToken.js
+++ src/getAccessToken/getAccessToken.js
@@ -4,12 +4,18 @@ const getRequestBody = () => ({
   client_id: process.env.MERCADO_PAGO_CLIENT_ID,
   client_secret: process.env.MERCADO_PAGO_CLIENT_SECRET,
   grant_type: 'client_credentials',
-
 });
 
 const getAccessToken = async () => {
-  const { data } = await axios.post(`${process.env.MERCADO_PAGO_OAUTH}`, getRequestBody());
-  return data.access_token;
+  let token = process.env.MERCADO_PAGO_ACCESS_TOKEN;
+  if (process.env.AWS_ENV === 'prod') {
+    const { data } = await axios.post(
+      `${process.env.MERCADO_PAGO_OAUTH}`,
+      getRequestBody(),
+    );
+    token = data.access_token;
+  }
+  return token;
 };
 
 export { getAccessToken };
